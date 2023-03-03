c35ee4370fa6005b141e68fb543ae52a9ed6e2b9
Muralidharan R <muralidharan@dooziesoft.com>
2023-03-03 14:34:34 +0530

    addeds section to create scheduler

--- resources/views/scheduler/scheduler_types/create.blade.php
+++ resources/views/scheduler/scheduler_types/create.blade.php
@@ -2,48 +2,56 @@
 @section('title', 'Dashboard')
 
 @section('content')
-<div class="container">
-    <div class="container-fluid pt-4 px-4" hidden>
-        <div class="row g-4">
-            <div class="col-sm-6 col-xl-3">
-                <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-list fa-3x text-primary"></i>
-                    <div class="ms-3">
-                        <p class="mb-2"></p>
-                        <h6 class="mb-0">No of Divisions</h6>
+    <div class="container">
+        <div class="container-fluid pt-4 px-4" hidden>
+            <div class="row g-4">
+                <div class="col-sm-6 col-xl-3">
+                    <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                        <i class="fa fa-list fa-3x text-primary"></i>
+                        <div class="ms-3">
+                            <p class="mb-2"></p>
+                            <h6 class="mb-0">No of Divisions</h6>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-microchip fa-3x text-primary"></i>
-                    <div class="ms-3">
-                        <p class="mb-2"></p>
-                        <h6 class="mb-0">No of Stations</h6>
+                <div class="col-sm-6 col-xl-3">
+                    <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                        <i class="fa fa-microchip fa-3x text-primary"></i>
+                        <div class="ms-3">
+                            <p class="mb-2"></p>
+                            <h6 class="mb-0">No of Stations</h6>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-user-pen fa-3x text-primary"></i>
-                    <div class="ms-3">
-                        <p class="mb-2"></p>
-                        <h6 class="mb-0">WIP</h6>
+                <div class="col-sm-6 col-xl-3">
+                    <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                        <i class="fa fa-user-pen fa-3x text-primary"></i>
+                        <div class="ms-3">
+                            <p class="mb-2"></p>
+                            <h6 class="mb-0">WIP</h6>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-scanner-gun fa-3x text-primary"></i>
-                    <div class="ms-3">
-                        <p class="mb-2"></p>
-                        <h6 class="mb-0">Scanned Today</h6>
+                <div class="col-sm-6 col-xl-3">
+                    <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                        <i class="fa fa-scanner-gun fa-3x text-primary"></i>
+                        <div class="ms-3">
+                            <p class="mb-2"></p>
+                            <h6 class="mb-0">Scanned Today</h6>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    {{-- <div class="container-fluid pt-4 px-4">
+
+        <section class="bg-light mt-4">
+            <div class="container">
+                <h2 class="lead-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, facilis?
+                </h2>
+            </div>
+        </section>
+        {{-- <div class="container-fluid pt-4 px-4">
         <div class="row g-4">
             @foreach ($station_heartbeats as $station_heartbeat)
             <div class="col-sm-6 col-xl-3">
@@ -58,7 +66,7 @@
             @endforeach --}}
 
 
-        </div>
     </div>
-</div>
+    </div>
+    </div>
 @endsection
